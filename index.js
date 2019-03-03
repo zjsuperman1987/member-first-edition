531e08e47fc4eda817502b7570b9e3a0633cb415
zyEros <395693101@qq.com>
2019-03-03 22:29:36 +0800

    home3.2

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 window.onload = function() {
-	memberHome.init_iscroll();
+    memberHome.initSwiper();
+    memberHome.init_iscroll();
 }
 
 
 memberHome = (function() {
     return {
-    	globSpec: {},
-    	init_iscroll: function () {
-			refresh.init('#glob_wrapper_iscroll');
-    	}
+        globSpec: {},
+        initSwiper: function() {
+            var headerSwiper = new Swiper('.swiper-container-header', {
+                autoplay: true,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+            })
+            axios.get('https://www.easy-mock.com/mock/5c77f974ee24c36460daaffb/example/index_data')
+                .then(function(response) {
+            		len = response.data.headerCarouse.length
+                    if (response.data.headerCarouse && len != 0) {
+                    	for (var i = 0; i < len; i++) {
+                    		headerSwiper.appendSlide('<div class="swiper-slide"><img src="' + response.data.headerCarouse[i].imgUrl + '"></div>');
+                    	}
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+
+        },
+        init_iscroll: function() {
+            refresher.init({
+                id: '#glob_wrapper_iscroll',
+                pullDownAction: function() {
+                    refresher.spec['#glob_wrapper_iscroll'].refresh();
+                },
+                pullUpAction: function() {
+                    refresher.spec['#glob_wrapper_iscroll'].refresh();
+                }
+            });
+        }
     }
 })();
--- js/iscroll/iscroll-pull-down-refresh.js
+++ js/iscroll/iscroll-pull-down-refresh.js
@@ -1,88 +1,169 @@
-var refresh = {
+var refresher = {
+    spec: {},
     info: {
-        "pullDownLable": "",
-        "pullingDownLable": "Release to refresh...",
-        "pullUpLable": "",
-        "pullingUpLable": "Release to load more...",
-        "loadingLable": "Loading..."
+        pullDownLable: '',
+        pullingDownLable: 'Release to refresh...',
+        pullUpLable: '',
+        pullingUpLable: 'Release to load more...',
+        loadingLable: 'Loading...'
     },
     init: function(parameter) {
-        var wrapper,
-            scroller,
-            pullDown,
-            pullDownLable,
-            pullUp,
-            pullUpLable,
-            pullDownEl,
-            pullDownOffset,
-            pullUpEl,
-            pullUpOffset,
-            content,
-            loader,
-            list,
-            div;
 
-        // 创建滚动div
-        wrapper = document.querySelector(parameter);
-        div = document.createElement('div');
-        div.className = 'scroller';
-        wrapper.appendChild(div);
-        // 插入UL
-        scroller = document.querySelector('.scroller');
-        list = document.querySelector('#' + wrapper.id + " ul");
-        scroller.appendChild(list);
-        // 创建下拉元素
-        pullDown = document.createElement('div');
-        pullDown.className = "pullDown";
-        loader = document.createElement('div');
-        loader.className = "loader";
-        for (var i = 0; i < 4; i++) {
+        // 初始scroller
+        var wrapper = document.querySelector(parameter.id);
+        var scrollerDIV = document.createElement('div');
+        scrollerDIV.classList.add('scroller');
+        wrapper.appendChild(scrollerDIV);
+        scrollerDIV = wrapper.querySelector('.scroller');
+        scrollerDIV.appendChild(wrapper.querySelector('ul'));
+
+        // 初始化下拉元素
+        var pullDownDIV = document.createElement('div');
+        pullDownDIV.classList.add('pullDown');
+        scrollerDIV.insertBefore(pullDownDIV, wrapper.querySelector('ul'));
+
+        var loaderDIV = document.createElement('div');
+        loaderDIV.classList.add('loader');
+        for (i = 0; i <= 4; i++) {
             var span = document.createElement('span');
-            loader.appendChild(span);
+            loaderDIV.appendChild(span);
         };
-        pullDown.appendChild(loader);
-        pullDownLable = document.createElement('div');
-        pullDownLable.className = 'pullDownLable';
-        pullDown.appendChild(pullDownLable);
-        scroller.insertBefore(pullDown, scroller.childNodes[0]);
+        pullDownDIV.appendChild(loaderDIV);
 
-        // 创建上拉元素
-        pullUp = document.createElement('div');
-        pullUp.className = 'pullUp';
-        loader = document.createElement('div')
-        loader.className = 'loader';
-        for (var i = 0; i < 4; i++) {
+        var pullDownLableDIV = document.createElement('div');
+        pullDownLableDIV.classList.add('pullDownLable');
+        pullDownDIV.appendChild(pullDownLableDIV)
+
+
+        // 初始化上拉元素
+        var pullUpDIV = document.createElement('div');
+        pullUpDIV.classList.add('pullUp');
+        scrollerDIV.appendChild(pullUpDIV, wrapper.querySelector('ul'));
+
+        var loaderDIV = document.createElement('div');
+        loaderDIV.classList.add('loader');
+        for (i = 0; i <= 4; i++) {
             var span = document.createElement('span');
-            loader.appendChild(span);
+            loaderDIV.appendChild(span);
         };
-        pullUp.appendChild(loader);
-        pullUpLable = document.createElement('div');
-        pullUpLable.className = 'pullUpLable';
-        content = document.createTextNode(refresh.info.pullUpLable);
-        pullUpLable.appendChild(content);
-        pullUp.appendChild(pullUpLable);
-        scroller.appendChild(pullUp);
+        pullUpDIV.appendChild(loaderDIV);
 
-        // 取出加载元素高度
-        pullDownEl = document.querySelector('.pullDown');
-        pullDownOffset = pullDownEl.offsetHeight;
-        pullUpEl = document.querySelector('.pullUp');
-        pullUpOffset = pullUpEl.offsetHeight;
-        this.scrollIt(wrapper, pullDownEl, pullDownOffset, pullUpEl, pullUpOffset);
+        var pullUpLableDIV = document.createElement('div');
+        pullUpLableDIV.classList.add('pullUpLable');
+        pullUpDIV.appendChild(pullUpLableDIV);
+
+        // 上拉 下拉 动画 高度
+        var pullDownOffset = pullDownDIV.offsetHeight;
+        var pullUpOffset = pullUpDIV.offsetHeight;
+
+
+        // 初始化iscroll
+        this.scrollIt(parameter, pullDownDIV, pullDownLableDIV, pullDownOffset, pullUpDIV, pullUpLableDIV, pullUpOffset);
+    },
+    scrollIt: function(parameter, pullDownDIV, pullDownLableDIV, pullDownOffset, pullUpDIV, pullUpLableDIV, pullUpOffset) {
+        var that = this;
+        this.spec[parameter.id] = new IScroll(parameter.id, {
+            probeType: 3,
+            minY: -pullUpOffset,
+            pullUpOffset: pullUpOffset,
+            onRelease: function() {
+                that.onRelease(this, pullDownDIV, pullDownLableDIV, parameter.pullDownAction, pullUpDIV, pullUpLableDIV);
+            }
+        });
+
+     
+
+        // 重置 resetPosition 方法
+        this.spec[parameter.id].resetPosition = function(time) {
+            var x = this.x,
+                y = this.y;
+
+            time = time || 0;
+
+            if (!this.hasHorizontalScroll || this.x > 0) {
+                x = 0;
+            } else if (this.x < this.maxScrollX) {
+                x = this.maxScrollX;
+            }
+
+            if (!this.hasVerticalScroll || this.y > this.options.minY) {
+                y = this.options.minY;
+            } else if (this.y < this.maxScrollY) {
+                y = this.maxScrollY;
+            }
+
+            if (x == this.x && y == this.y) {
+                return false;
+            }
+
+            this.scrollTo(x, y, time, this.options.bounceEasing);
+
+            return true;
+        };
+        this.spec[parameter.id].resetPosition();
+
+
+        // 滚动中
+        this.spec[parameter.id].on('scroll', function() {
+            var y = this.y >> 0;
+
+            //======================== 下拉动作 ========================
+            // 开始下拉 -40 pullDownOffset
+            if (y > -pullDownOffset && !pullDownDIV.classList.contains('loading')) {
+                pullDownLableDIV.innerHTML = that.info.pullingDownLable;
+            }
+
+            // 上拉 maxY
+            if (y < this.maxScrollY && !pullUpDIV.classList.contains('loading')) {
+                pullUpLableDIV.innerHTML = that.info.pullingUpLable;
+            }
+
+        });
+        // 滚动结束
+        this.spec[parameter.id].on('scrollEnd', function () {
+            if (pullDownDIV.className.match('loading')){
+                if (parameter.pullDownAction) parameter.pullDownAction();
+            }
+            if (pullUpDIV.className.match('loading')){
+                if (parameter.pullUpAction) parameter.pullUpAction();
+            } 
+        })
+        // 刷新
+        this.spec[parameter.id].on('refresh', function () {
+            if (pullDownDIV.className.match('loading')){
+                pullDownDIV.classList.toggle('loading');
+                pullDownDIV.style.lineHeight = '40px';
+                pullDownDIV.querySelector('.loader').style.display = 'none';
+                pullDownLableDIV.innerHTML = refresher.info.pullDownLable;
+                this.options.minY = -pullDownOffset;
+            }
+            if (pullUpDIV.className.match('loading')){
+                pullUpDIV.classList.toggle('loading');
+                pullUpDIV.style.lineHeight = '40px';
+                pullUpDIV.querySelector('.loader').style.display = 'none';
+                pullUpLableDIV.innerHTML = refresher.info.pullUpLable;
+                // this.maxScrollY = this.wrapperHeight - this.scrollerHeight +pullUpOffset;
+            }
+        })
 
     },
-    scrollIt: function(wrapper, pullDownEl, pullDownOffset, pullUpEl, pullUpOffset) {
-        this.info[wrapper.id] = new IScroll(wrapper, {
-            probeType: 3,
-            bounce: false,
-        });
-        this.info[wrapper.id].on('scroll', function() {
-        	 var y = this.y >> 0;
-        	 console.log(y);
+    onRelease: function(scroller, pullDownDIV, pullDownLableDIV, pullDownAction, pullUpDIV, pullUpLableDIV) {
+        var y = scroller.y >> 0;
 
-        })
-        this.info[wrapper.id].on('scrollEnd', function() {
+        if (y > 0) {
+            scroller.options.minY = 0;
+            pullDownDIV.style.lineHeight = '20px';
+            pullDownDIV.querySelector('.loader').style.display = 'block';
+            pullDownLableDIV.innerHTML = refresher.info.loadingLable;
+            pullDownDIV.classList.add('loading');
+        }
 
-        })
+        if (y < scroller.maxScrollY - pullUpDIV.offsetHeight) {
+            scroller.maxScrollY = scroller.wrapperHeight - scroller.scrollerHeight;
+            pullUpDIV.style.lineHeight = '20px';
+            pullUpDIV.querySelector('.loader').style.display = 'block';
+            pullUpLableDIV.innerHTML = refresher.info.loadingLable;
+            pullUpDIV.classList.add('loading');
+        }
     }
 }
